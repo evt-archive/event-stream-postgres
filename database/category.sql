1687258e60e0e338a748eecf3af7bbea8fed9cc9
Scott Bellware <sbellware@gmail.com>
2016-05-15 19:24:53 -0500

    Database scripts and binaries

--- database/category.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION category(
-  stream_name varchar
-)
-RETURNS varchar
-AS $$
-BEGIN
-  return split_part(stream_name, '-', 1);
-END;
-$$ LANGUAGE plpgsql;
--- database/test_write_event_expected_version.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
-
-SELECT write_event(gen_random_uuid(), 'SomeType', 'someStream-123', '{"someField":"some value"}', '{"someMetadataField":"some metadata value"}', 1);
