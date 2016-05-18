d62e7bc7f35199db938ce7c00bc1f8e1af5fb20c
Scott Bellware <sbellware@gmail.com>
2016-05-17 19:28:31 -0500

    Superfluous test implementation is removed

--- test/bench/write/write_event.rb
+++ test/bench/write/write_event.rb
@@ -6,12 +6,8 @@ context "Write Event Data" do
   stream_name = controls::StreamName.example
 
   write_event = controls::EventData::Write.example
-  __logger.focus write_event.inspect
-
 
   stream_version = EventStream::Postgres::Write.(stream_name, write_event)
-
-  __logger.focus stream_version
 
   test "Result is stream version" do
     refute(stream_version.nil?)
