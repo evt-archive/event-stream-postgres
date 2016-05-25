65cf8273785a3b538f5e66f9fbc37f1c2bc23cd2
Scott Bellware <sbellware@gmail.com>
2016-05-25 18:24:31 -0500

    Select statement defaults are tested

--- test/bench/select_statement/defaults.rb
+++ test/bench/select_statement/defaults.rb
@@ -10,10 +10,24 @@ context "Select Statement" do
 
     get = Get::SelectStatement.build stream
 
-    context "Stream position" do
+    context "Stream Position" do
       default_stream_position = Get::SelectStatement::Defaults.stream_position
       test "#{default_stream_position}" do
         assert(get.stream_position == default_stream_position)
+      end
+    end
+
+    context "Batch Size" do
+      default_batch_size = Get::SelectStatement::Defaults.batch_size
+      test "#{default_batch_size}" do
+        assert(get.batch_size == default_batch_size)
+      end
+    end
+
+    context "Precedence" do
+      default_precedence = Get::SelectStatement::Defaults.precedence
+      test "#{default_precedence}" do
+        assert(get.precedence == default_precedence)
       end
     end
   end
