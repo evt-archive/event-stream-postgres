5a510e0a06dedd15fb8da958af9da19c0f94f327
Scott Bellware <sbellware@gmail.com>
2016-05-27 16:47:47 -0500

    select_statement, rather than get

--- test/bench/select_statement/defaults.rb
+++ test/bench/select_statement/defaults.rb
@@ -6,26 +6,26 @@ context "Select Statement" do
   context "Defaults" do
     stream = controls::Stream.example
 
-    get = Get::SelectStatement.build stream
+    select_statement = Get::SelectStatement.build stream
 
     context "Stream Position" do
       default_stream_position = Get::SelectStatement::Defaults.stream_position
       test "#{default_stream_position}" do
-        assert(get.stream_position == default_stream_position)
+        assert(select_statement.stream_position == default_stream_position)
       end
     end
 
     context "Batch Size" do
       default_batch_size = Get::SelectStatement::Defaults.batch_size
       test "#{default_batch_size}" do
-        assert(get.batch_size == default_batch_size)
+        assert(select_statement.batch_size == default_batch_size)
       end
     end
 
     context "Precedence" do
       default_precedence = Get::SelectStatement::Defaults.precedence
       test "#{default_precedence}" do
-        assert(get.precedence == default_precedence)
+        assert(select_statement.precedence == default_precedence)
       end
     end
   end
