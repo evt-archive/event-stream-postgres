2bfef94a3b226ce546192b50af23b1c68a12a941
Scott Bellware <sbellware@gmail.com>
2016-05-27 19:52:02 -0600

    Test for category query

--- test/bench/get/category.rb
+++ test/bench/get/category.rb
@@ -4,8 +4,9 @@ controls = EventStream::Postgres::Controls
 
 context "Get" do
   context "Category" do
-    category = 'some_category'
+    category = controls::Category.example randomize_category: true
 
+    # remove randomize cat
     stream_name_1 = controls::StreamName.example randomize_category: false, category: category
     controls::Put.(stream_name: stream_name_1)
 
@@ -14,11 +15,9 @@ context "Get" do
 
     events = Get.(category: category)
 
-    # test "Number of events retrieved is the number written to the category" do
-    #   number_of_events = events.length
-    #   assert(number_of_events == 2)
-    # end
-
-    test "Number of events retrieved is the number written to the category"
+    test "Number of events retrieved is the number written to the category" do
+      number_of_events = events.length
+      assert(number_of_events == 2)
+    end
   end
 end
