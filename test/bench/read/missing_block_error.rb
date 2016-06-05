5bb841c0498d37c607a9ffcabc83e88365566d2c
Scott Bellware <sbellware@gmail.com>
2016-06-05 13:33:17 -0700

    Missing test block

--- test/bench/read/missing_block_error.rb
+++ test/bench/read/missing_block_error.rb
@@ -4,8 +4,10 @@ controls = EventStream::Postgres::Controls
 
 context "Read" do
   context "No block supplied" do
-    assert proc { Read.(stream_name: 'some_stream_name') } do
-      raises_error? Read::Error
+    test "Is incorrect" do
+      assert proc { Read.(stream_name: 'some_stream_name') } do
+        raises_error? Read::Error
+      end
     end
   end
 end
