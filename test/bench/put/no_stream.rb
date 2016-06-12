e2a58b485c3254db9d996e5268aa7185c52f3683
Scott Bellware <sbellware@gmail.com>
2016-06-11 17:23:18 -0700

    Reminder to implement no_stream

--- test/bench/put/no_stream.rb
+++ test/bench/put/no_stream.rb
@@ -1,4 +1,6 @@
-raise 'TODO: Implement no stream tests'
+require_relative '../bench_init'
+
+test 'TODO: Implement no stream tests'
 # no_stream turns into -1
 # test that new stream is new stream
 
