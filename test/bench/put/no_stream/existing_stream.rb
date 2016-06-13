94396b05d534f013e7e2fe7a07d84470bd23fedf
Scott Bellware <sbellware@gmail.com>
2016-06-13 00:36:28 -0700

    Tests for no stream and assurance of first event in stream

---
+++ test/bench/put/no_stream/existing_stream.rb
@@ -0,0 +1,18 @@
+require_relative '../../bench_init'
+
+controls = EventStream::Postgres::Controls
+
+context "Put" do
+  context "No Stream" do
+    context "Existing Stream" do
+      stream_name = controls::StreamName.example
+      event = controls::EventData::Write.example
+
+      put = Put.build(stream_name, event, expected_version: NoStream.name)
+
+      test "Sets the expected version to -1" do
+        assert(put.expected_version == NoStream.version)
+      end
+    end
+  end
+end
