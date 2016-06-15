3cae89eb34e14621614b428aff7bd79e40591a3f
Scott Bellware <sbellware@gmail.com>
2016-06-14 23:49:30 -0700

    Iterator tests are in their own files, and a specific suite for the iterator

--- test/bench/iterator/next.rb
+++ test/bench/iterator/next.rb
@@ -19,18 +19,4 @@ context "Iterator" do
       assert(batch.length == 2)
     end
   end
-
-  context "No further event data" do
-    stream_name = controls::Put.(instances: 2)
-
-    iterator = Iterator.build(stream_name: stream_name)
-
-    2.times { iterator.next }
-
-    last = iterator.next
-
-    test "Results in nil" do
-      assert(last.nil?)
-    end
-  end
 end
